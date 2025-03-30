fa83bb979e4aa6d8c1973c0acab64bb1729607c4
RAXAS <raxas952@gmail.com>
2025-03-30 16:52:22 +0300

    upgrade README

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,7 +1,8 @@
-user_list = [9, 5, 4, 2, 6, 3]
+user_list = list(map(int, input("Введите цифры через запятую: ").split(',')))
+print(f"Список до сортировки: {user_list}")
 n = len(user_list)
 for i in range(n - 1):
     for j in range(n - 1 - i):
         if user_list[j] > user_list[j + 1]:
             user_list[j], user_list[j + 1] = user_list[j + 1], user_list[j]
-print(user_list)
+print(f"Список после сортировки: {user_list}")
